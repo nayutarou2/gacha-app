16ecb3a5f24f09d831cdcf8bc7abb5c5a311ea84
akira <a.ono@infomart.co.jp>
2026-06-01 10:35:36 +0900

    ResultDetailで指摘された部分の改善
    描画レンダリングの中のmapの中でcountを加算しているところをredureを使いレンダリング前にtotalCountに計算結果を入れ修正

--- gacha-app-frontend/components/ResultDetail.tsx
+++ gacha-app-frontend/components/ResultDetail.tsx
@@ -1,34 +1,24 @@
 "use client"
 import styles from "@/components/ResultDetail.module.css";
-import { ResultData } from "@/app/interface/ResultData";
-import { useState } from "react";
 
 interface ResultCountList {
   resultCount: number[] | undefined;
 }
 
-type Count = {
-  data:number[];
-}
-
 export default function ResultDetail(props: ResultCountList) {
 
-  const [results, setResults] = useState<ResultData | null>(null);
+  const resultsList = props.resultCount || [];
 
-  const resultsList = props.resultCount;
-  console.log("reusltsList:",resultsList);
+  // reduceを使い、配列の要素を左から右へ順番に処理する
+  const totalCount = resultsList.reduce((acc, curr) => acc + curr, 0);
 
-  let count = 0;
-  
-  const textChange = (index:number,data:number) =>{
-    count += data;
-    let text = "";
+  const textChange = (index: number) => {
     switch (index) {
-      case 0: return text = "S";
-      case 1: return text = "A";
-      case 2: return text = "B";
-      case 3: return text = "C";
-      default: return text;
+      case 0: return "S";
+      case 1: return "A";
+      case 2: return "B";
+      case 3: return "C";
+      default: return "";
     }
   }
 
@@ -36,14 +26,14 @@ export default function ResultDetail(props: ResultCountList) {
   return (
     <div className={styles.result_count}>
       {
-        resultsList?.map((data,index) => (
+        resultsList.map((data, index) => (
           <div key={index}>
-            <div className={styles.shadow}> {textChange(index,data)} : <span>{data}</span>回</div>
+            <div className={styles.shadow}> {textChange(index)} : <span>{data}</span>回</div>
           </div>
         ))
       }
       <div>
-        合計 : <span>{count}回</span>
+        合計 : <span>{totalCount}回</span>
       </div>
     </div>
   )
