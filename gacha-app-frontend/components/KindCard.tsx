2cdb7ef2a5b346bba222ff73b980977cdffd89dd
akira <a.ono@infomart.co.jp>
2026-06-02 10:47:48 +0900

    Cardで統一していたものを二種類に分離

---
+++ gacha-app-frontend/components/KindCard.tsx
@@ -0,0 +1,39 @@
+'use client';
+import styles from '@/components/KindCard.module.css';
+import { KindsData } from '@/app/interface/KindsData';
+import { pullGacha } from '@/app/api/pull';
+import { useRouter } from 'next/navigation';
+
+// propsでそれぞれのコンポーネントから値を受け取る
+interface KindProps {
+  response: KindsData[];
+}
+
+export default function KindsCard(props: KindProps) {
+  const router = useRouter();
+
+  const onClickKindHandler = async (kindNum: number) => {
+    try {
+      const response = await pullGacha(kindNum);
+      console.log('レスポンス:', response);
+      console.log("result?",JSON.stringify(response.gachaResult));
+      localStorage.setItem("gachaResult",JSON.stringify(response.gachaResult));
+      router.push(`/gacha/result/${response.id}`);
+    } catch (error) {
+      console.error('通信失敗 : ', error);
+    }
+  };
+
+  return (
+    <>
+      {props.response.map((data) => (
+        <div key={data.id}>
+          <div className={styles.kind_card} onClick={() => onClickKindHandler(data.kindsNum)}>
+            {data.kindsNum}
+          </div>
+          <div className={styles.shadow}></div>
+        </div>
+      ))}
+    </>
+  );
+}
--- gacha-app-frontend/components/ResultDetail.tsx
+++ gacha-app-frontend/components/ResultDetail.tsx
@@ -1,42 +1,46 @@
-"use client"
-import styles from "@/components/ResultDetail.module.css";
+import styles from '@/components/ResultDetail.module.css';
 
 interface ResultCountList {
   resultCount: number[] | undefined;
 }
 
 export default function ResultDetail(props: ResultCountList) {
-
   const resultsList = props.resultCount || [];
+
+  console.log("resultList", resultsList);
 
   // reduceを使い、配列の要素を左から右へ順番に処理する
   const totalCount = resultsList.reduce((acc, curr) => acc + curr, 0);
 
   const textChange = (index: number) => {
     switch (index) {
-      case 0: return "S";
-      case 1: return "A";
-      case 2: return "B";
-      case 3: return "C";
-      default: return "";
+      case 0:
+        return 'S';
+      case 1:
+        return 'A';
+      case 2:
+        return 'B';
+      case 3:
+        return 'C';
+      default:
+        return '';
     }
-  }
-
+  };
 
   return (
-    <div className={styles.result_count}>
-      {
-        resultsList.map((data, index) => (
+    <>
+      <div className={styles.results}>
+        {resultsList.map((data, index) => (
           <div key={index}>
-            <div className={styles.shadow}> {textChange(index)} : <span>{data}</span>回</div>
+            <div className={""}>
+              {textChange(index)} : <span>{data}</span>回
+            </div>
           </div>
-        ))
-      }
-      <div>
-        合計 : <span>{totalCount}回</span>
+        ))}
+        <div>
+          合計 : <span>{totalCount}回</span>
+        </div>
       </div>
-    </div>
-  )
-
-
+    </>
+  );
 }
