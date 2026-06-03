dc72074b41bfd9e68a9c3fba3190019d72584515
akira <a.ono@infomart.co.jp>
2026-06-03 15:39:10 +0900

    エラーハンドラーの実装 400,401,404,422,500

--- gacha-app-backend/src/main/java/com/example/gacha_app_backend/exception/ResourceNotFoundException.java
+++ gacha-app-backend/src/main/java/com/example/gacha_app_backend/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.gacha_app_backend.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
   
-  // 後々messages.propertiesに移行 
+  // 404
   public ResourceNotFoundException(String resourceName,Long id){
     super(String.format("%s (ID:%d)が見つかりませんでした。",resourceName, id));
   }
