254666b5cbb60630e01c3660300d1a3f36bc1797
akira <a.ono@infomart.co.jp>
2026-06-08 13:39:30 +0900

    登録時にUserDtoのバリデーションチェックが自動適用されるようにValidを追加

--- gacha-app-backend/src/main/java/com/example/gacha_app_backend/controller/UserController.java
+++ gacha-app-backend/src/main/java/com/example/gacha_app_backend/controller/UserController.java
@@ -12,6 +12,8 @@ import com.example.gacha_app_backend.dto.UserDto;
 import com.example.gacha_app_backend.service.AuthService;
 import com.example.gacha_app_backend.service.UserService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
@@ -26,7 +28,7 @@ public class UserController {
 
   // user登録
   @PostMapping("/register")
-  public ResponseEntity<Integer> createUser(@RequestBody UserDto userDto) {
+  public ResponseEntity<Integer> createUser(@RequestBody @Valid UserDto userDto) {
 
     return ResponseEntity.ok(userService.insertUser(userDto));
 
