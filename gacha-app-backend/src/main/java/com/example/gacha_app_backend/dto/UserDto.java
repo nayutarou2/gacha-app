1fffd764e2bbd3498db6430924c0c243975e9509
akira <a.ono@infomart.co.jp>
2026-06-08 13:38:36 +0900

    UserDtoにバリデーションの追加

--- gacha-app-backend/src/main/java/com/example/gacha_app_backend/dto/UserDto.java
+++ gacha-app-backend/src/main/java/com/example/gacha_app_backend/dto/UserDto.java
@@ -1,13 +1,23 @@
 package com.example.gacha_app_backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDto {
-  
+
+  @NotBlank(message = "ユーザー名は必須です")
+  @Size(max = 10, message = "ユーザー名は10文字以内で入力してください")
   private String username;
+  @NotBlank(message = "メールアドレスは必須です")
+  @Email(message = "メールアドレスの形式が正しくありません")
   private String email;
+  @NotBlank(message = "パスワードは必須です")
+  @Size(min = 8, message = "パスワードは8文字以上で入力してください")
   private String password;
+  @NotBlank(message = "パスワード(確認)は必須です")
   private String passwordConfirm;
 
 }
